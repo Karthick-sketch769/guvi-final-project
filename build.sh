276b1f849e19ec78de88faa77e34cac5d2602570
root <root@ip-172-31-45-92.ap-southeast-2.compute.internal>
2024-12-05 06:23:17 +0000

    prod

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker build -t karthick997/dev:cappro .
+sudo docker build -t karthick997/prod:cappro .
 sudo docker images
 sudo docker-compose up -d --remove-orphans
 sudo docker ps -a
