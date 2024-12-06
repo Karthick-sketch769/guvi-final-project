259982f62c075d1c82a271b469081d10b40841ad
root <root@ip-172-31-45-92.ap-southeast-2.compute.internal>
2024-12-06 05:43:10 +0000

    recent

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+docker login -u karthick997 -p dckr_pat_Ir4BWXuJTfgAbsHhauMcWatLbFU
 sudo docker build -t karthick997/dev:cappro .
 sudo docker images
 sudo docker-compose up -d --remove-orphans
