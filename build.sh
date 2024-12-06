ead0fb882ec67a19177b43d0ecd51f5340ae4565
Karthick-sketch769 <karthicksethu189@gmail.com>
2024-12-06 13:19:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker login -u karthick997 -p dckr_pat_Ir4BWXuJTfgAbsHhauMcWatLbFU
-sudo docker build -t karthick997/prod:cappro .
+sudo docker build -t karthick997/dev:cappro .
 sudo docker images
 sudo docker-compose up -d --remove-orphans
 sudo docker ps -a
